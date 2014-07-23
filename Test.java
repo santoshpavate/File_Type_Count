89541b4c42ea857a03aaa71a44ebf697ae944cc2
santoshpavate <santosh_pavate@hotmail.com>
2014-07-23 18:09:06 +0530

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,17 @@
+public class Test {
+
+	public static void main(String[] args) {
+
+		System.out.println("App Start");
+
+		// Specify Directory here (folder Name)
+		String file = "D:\\JAVA - Jspiders";
+
+		Implementation implement = new Implementation(file);
+		implement.takeUserInput();
+
+		System.out.println("App End");
+
+	}
+
+}
